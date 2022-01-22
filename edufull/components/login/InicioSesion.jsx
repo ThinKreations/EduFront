95c1f82e780644b9b294130795fed619a817d119
addRian0-0 <addrianun@gmail.com>
2022-01-21 21:32:35 -0600

    nomas hice mamadas

---
+++ edufull/components/login/InicioSesion.jsx
@@ -0,0 +1,12 @@
+import styles from '../../styles/Login.module.css'
+
+export default function InicioSesion () {
+  return (
+    <>
+      <div>
+        <h4 className={styles.sub}>Correo:</h4>
+        <input type="text" />
+      </div>
+    </>
+  )
+}
---
+++ edufull/pages/login/index.jsx
@@ -0,0 +1,12 @@
+import Container from "../../components/Container";
+import ContainerForm from "../../components/login/ContainerForm";
+
+//Inicio de sesion
+export default function Index () {
+  return (
+    <>
+      <Container title="Inicio de sesión." />
+      <ContainerForm titulo="Inicio de sesión."/>
+    </>
+  )
+}
